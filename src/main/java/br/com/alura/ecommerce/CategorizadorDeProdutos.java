b6654faea6c34a104a044fce594fa8ff2ee181f1
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-11-23 23:49:38 -0300

    3.3 Prompt template

--- src/main/java/br/com/alura/ecommerce/CategorizadorDeProdutos.java
+++ src/main/java/br/com/alura/ecommerce/CategorizadorDeProdutos.java
@@ -7,28 +7,41 @@ import com.theokanning.openai.service.OpenAiService;
 
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class CategorizadorDeProdutos {
 
     public static void main(String[] args) {
-        var user = "Celular";
-        var system = """
-        Você é um categorizador de produtos e deve responder apenas o nome da categoria do produto informado
-        
-        Escolha uma categoria dentra a lista abaixo:
-        
-        1. Higiene pessoal
-        2. Eletronicos
-        3. Esportes
-        4. Outros
-        
-        ###### exemplo de uso:
-        
-        Pergunta: Bola de futebol
-        Resposta: Esportes
-        """;
+        var leitor = new Scanner(System.in);
 
+        System.out.println("Digite as categorias válidas:");
+        var categorias = leitor.nextLine();
 
+        while(true) {
+            System.out.println("\nDigite o nome do produto:");
+            var user = leitor.nextLine();
+
+            var system = """
+                    Você é um categorizador de produtos e deve responder apenas o nome da categoria do produto informado
+                                
+                    Escolha uma categoria dentra a lista abaixo:
+                                
+                    %s
+                                
+                    ###### exemplo de uso:
+                                
+                    Pergunta: Bola de futebol
+                    Resposta: Esportes
+                    
+                    ###### regras a serem seguidas:
+                    Caso o usuario pergunte algo que nao seja de categorizacao de produtos, voce deve responder que nao pode ajudar pois o seu papel é apenas responder a categoria dos produtos
+                    """.formatted(categorias);
+
+            dispararRequisicao(user, system);
+        }
+    }
+
+    public static void dispararRequisicao(String user, String system) {
         var chave = System.getenv("OPENAI_API_KEY");
         var service = new OpenAiService(chave, Duration.ofSeconds(30));
 
@@ -39,16 +52,12 @@ public class CategorizadorDeProdutos {
                         new ChatMessage(ChatMessageRole.USER.value(), user),
                         new ChatMessage(ChatMessageRole.SYSTEM.value(), system)
                 ))
-                .n(5)
                 .build();
 
         service
                 .createChatCompletion(completionRequest)
                 .getChoices()
-                .forEach(c -> {
-                    System.out.print(c.getMessage().getContent());
-                    System.out.println("\n------------------------\n");
-                });
+                .forEach(c -> System.out.print(c.getMessage().getContent()));
     }
 
 }
