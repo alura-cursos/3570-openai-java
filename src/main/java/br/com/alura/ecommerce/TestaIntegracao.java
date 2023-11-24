df97cc76294f926c8b9dfaf0fba668f6fad2e067
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-11-23 22:31:50 -0300

    2.1 - Criacao do projeto

---
+++ src/main/java/br/com/alura/ecommerce/TestaIntegracao.java
@@ -0,0 +1,12 @@
+package br.com.alura.ecommerce;
+
+public class TestaIntegracao {
+
+    public static void main(String[] args) {
+        var user = "Gere 5 produtos";
+        var system = "Você é um gerador de produtos ficticios para um ecommerce e deve gerar apenas o nome dos produtos solicitados pelo usuário";
+
+
+    }
+
+}
