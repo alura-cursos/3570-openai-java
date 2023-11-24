fc1d7d6132d05c12b7c636510fbe232cef2aa2f5
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-11-23 22:58:09 -0300

    2.2 - Integracao com a API

--- src/main/java/br/com/alura/ecommerce/TestaIntegracao.java
+++ src/main/java/br/com/alura/ecommerce/TestaIntegracao.java
@@ -1,12 +1,34 @@
 package br.com.alura.ecommerce;
 
+import com.theokanning.openai.completion.CompletionRequest;
+import com.theokanning.openai.completion.chat.ChatCompletionRequest;
+import com.theokanning.openai.completion.chat.ChatMessage;
+import com.theokanning.openai.completion.chat.ChatMessageRole;
+import com.theokanning.openai.service.OpenAiService;
+
+import java.util.Arrays;
+
 public class TestaIntegracao {
 
     public static void main(String[] args) {
         var user = "Gere 5 produtos";
         var system = "Você é um gerador de produtos ficticios para um ecommerce e deve gerar apenas o nome dos produtos solicitados pelo usuário";
 
+        var service = new OpenAiService("your_token");
 
+        var completionRequest = ChatCompletionRequest
+                .builder()
+                .model("gpt-4")
+                .messages(Arrays.asList(
+                        new ChatMessage(ChatMessageRole.USER.value(), user),
+                        new ChatMessage(ChatMessageRole.SYSTEM.value(), system)
+                ))
+                .build();
+
+        service
+                .createChatCompletion(completionRequest)
+                .getChoices()
+                .forEach(System.out::println);
     }
 
 }
