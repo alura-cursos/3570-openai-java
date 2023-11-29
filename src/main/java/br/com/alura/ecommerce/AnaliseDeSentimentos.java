0c7148a9a741fc3fe4a9b85c9d6ce443e9be4d16
Rodrigo Caneppele <r.caneppele@gmail.com>
2023-11-28 23:52:48 -0300

    5.2 - Chamada em lote

--- src/main/java/br/com/alura/ecommerce/AnaliseDeSentimentos.java
+++ src/main/java/br/com/alura/ecommerce/AnaliseDeSentimentos.java
@@ -10,53 +10,65 @@ import java.nio.file.Path;
 import java.nio.file.StandardOpenOption;
 import java.time.Duration;
 import java.util.Arrays;
+import java.util.stream.Collectors;
 
 public class AnaliseDeSentimentos {
 
     public static void main(String[] args) {
-        var promptSistema = """
-                Você é um analisador de sentimentos de avaliações de produtos.
-                Escreva um parágrafo com até 50 palavras resumindo as avaliações e depois atribua qual o sentimento geral para o produto.
-                Identifique também 3 pontos fortes e 3 pontos fracos identificados a partir das avaliações.
-                                
-                #### Formato de saída
-                Nome do produto:
-                Resumo das avaliações: [resuma em até 50 palavras]
-                Sentimento geral: [deve ser: POSITIVO, NEUTRO ou NEGATIVO]
-                Pontos fortes: [3 bullets points]
-                Pontos fracos: [3 bullets points]
-                """;
+        try {
+            var promptSistema = """
+                    Você é um analisador de sentimentos de avaliações de produtos.
+                    Escreva um parágrafo com até 50 palavras resumindo as avaliações e depois atribua qual o sentimento geral para o produto.
+                    Identifique também 3 pontos fortes e 3 pontos fracos identificados a partir das avaliações.
+                                    
+                    #### Formato de saída
+                    Nome do produto:
+                    Resumo das avaliações: [resuma em até 50 palavras]
+                    Sentimento geral: [deve ser: POSITIVO, NEUTRO ou NEGATIVO]
+                    Pontos fortes: [3 bullets points]
+                    Pontos fracos: [3 bullets points]
+                    """;
 
-        var produto = "tapete-de-yoga";
+            var diretorioAvaliacoes = Path.of("src/main/resources/avaliacoes");
+            var arquivosDeAvaliacoes = Files
+                    .walk(diretorioAvaliacoes, 1)
+                    .filter(path -> path.toString().endsWith(".txt"))
+                    .collect(Collectors.toList());
 
-        var promptUsuario = carregarArquivo(produto);
+            for (Path arquivo : arquivosDeAvaliacoes) {
+                System.out.println("Iniciando analise do produto: " +arquivo.getFileName());
+                var promptUsuario = carregarArquivo(arquivo);
 
-        var request = ChatCompletionRequest
-                .builder()
-                .model("gpt-4-1106-preview")
-                .messages(Arrays.asList(
-                        new ChatMessage(
-                                ChatMessageRole.SYSTEM.value(),
-                                promptSistema),
-                        new ChatMessage(
-                                ChatMessageRole.USER.value(),
-                                promptUsuario)))
-                .build();
+                var request = ChatCompletionRequest
+                        .builder()
+                        .model("gpt-4-1106-preview")
+                        .messages(Arrays.asList(
+                                new ChatMessage(
+                                        ChatMessageRole.SYSTEM.value(),
+                                        promptSistema),
+                                new ChatMessage(
+                                        ChatMessageRole.USER.value(),
+                                        promptUsuario)))
+                        .build();
 
-        var chave = System.getenv("OPENAI_API_KEY");
-        var service = new OpenAiService(chave, Duration.ofSeconds(60));
+                var chave = System.getenv("OPENAI_API_KEY");
+                var service = new OpenAiService(chave, Duration.ofSeconds(60));
 
-        var resposta = service
-                .createChatCompletion(request)
-                .getChoices().get(0).getMessage().getContent();
+                var resposta = service
+                        .createChatCompletion(request)
+                        .getChoices().get(0).getMessage().getContent();
 
-        salvarAnalise(produto, resposta);
+                salvarAnalise(arquivo.getFileName().toString().replace(".txt", ""), resposta);
+                System.out.println("Analise finalizada");
+            }
+        } catch (Exception e) {
+            System.out.println("Ocorreu um erro ao realizar as analises de sentimentos!");
+        }
     }
 
-    private static String carregarArquivo(String arquivo) {
+    private static String carregarArquivo(Path arquivo) {
         try {
-            var path = Path.of("src/main/resources/avaliacoes/avaliacoes-" +arquivo +".txt");
-            return Files.readAllLines(path).toString();
+            return Files.readAllLines(arquivo).toString();
         } catch (Exception e) {
             throw new RuntimeException("Erro ao carregar o arquivo!", e);
         }
